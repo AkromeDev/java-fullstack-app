c4abef98cccdbe7943ed63def0f9c6759a127088
JCBackendDev <jcbackenddev@gmail.com>
2023-02-11 18:25:44 +0100

    First version of the controller done
    that should give us a few little possibilities don't you think!?

---
+++ src/main/java/com/todo/javafullstack/controllers/todoController.java
@@ -0,0 +1,55 @@
+package com.todo.javafullstack.controllers;
+
+import com.todo.javafullstack.models.Todo;
+//import com.todo.javafullstack.respositories.TodoJpaRepository;
+import com.todo.javafullstack.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+public class todoController {
+
+    @Autowired
+    private TodoService todoService;
+
+//    @Autowired
+//    private TodoJpaRepository todoJpaRepository;
+
+    @GetMapping
+    public void createTable() {
+        todoService.createTodoTable();
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> getAll() {
+        return todoService.getAll();
+    }
+
+    @GetMapping("/todo")
+    public Todo getTodoById(@RequestParam Long id) {
+//        return todoJpaRepository.findById(id).get();
+        return todoService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void postTodos(@RequestBody List<Todo> todoList) {
+        todoService.postTodos(todoList);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void createTodo(@RequestBody Todo todo) {
+        todoService.createTodo(todo);
+    }
+}
