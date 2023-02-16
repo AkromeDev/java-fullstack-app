12050cafc6bf746e529c75be677b2fb749cb0949
JCBackendDev <jcbackenddev@gmail.com>
2023-02-16 22:36:13 +0100

    Added input and date fields

--- todo-ui/src/app/components/input/input.component.ts
+++ todo-ui/src/app/components/input/input.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatDatepickerModule ],
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
