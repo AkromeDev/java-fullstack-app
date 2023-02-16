510c9be4aa9f3528f5f609713120994c1ab071fd
JCBackendDev <jcbackenddev@gmail.com>
2023-02-16 22:36:57 +0100

    every endpoints is speaking successfully to the backend, no biggy!

--- todo-ui/src/app/services/todo.service.ts
+++ todo-ui/src/app/services/todo.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  todos: any[] = [
+    {title: "postInitialTodos", description: 'Hydrogen', dueDate: new Date("2018-01-22"), completionDate: new Date("2018-01-22")},
+    {title: "postInitialTodos", description: 'Helium', dueDate: new Date("2023-01-05"), completionDate: new Date("2023-02-04")},
+    {title: "postInitialTodos", description: 'Lithium', dueDate: new Date("2023-01-05"), completionDate: new Date("2023-02-04")},
+  ];
+
+  todo: any = 
+    {title: "1", 
+    description: 'Hydrogen', 
+    dueDate: new Date("2018-01-22"), 
+    completionDate: new Date("2018-01-22")};
+
   constructor(private http : HttpClient) { }
 
   getTodos() {
     return this.http.get('/server/app/todos')
   }
+
+  postInitialTodos() {
+    return this.http.post('/server/app/todoList', this.todos)
+  }
+
+  postTodo(todos: any) {
+    return this.http.post('/server/app/todos', todos)
+  }
 }
+
+
