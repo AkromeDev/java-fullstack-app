74a25cf6b1c22f087d588bb6b31680a454d95a81
JCBackendDev <jcbackenddev@gmail.com>
2023-02-19 23:11:09 +0100

    Experimenting with JPA repository

---
+++ src/main/java/com/todo/javafullstack/respositories/TodoJpaRepository.java
@@ -0,0 +1,7 @@
+//package com.todo.javafullstack.respositories;
+//
+//import com.todo.javafullstack.models.Todo;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface TodoJpaRepository extends JpaRepository <Todo, Long> {
+//}
