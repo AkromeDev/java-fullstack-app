50ecb1742924a8478ca25d4aa81033c1812e2a27
JCBackendDev <jcbackenddev@gmail.com>
2023-02-24 19:13:46 +0100

    new version of the input component, it is compiling fine but still have some error afterward

--- todo-ui/src/app/components/input/input.component.ts
+++ todo-ui/src/app/components/input/input.component.ts
@@ -1,16 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
 // import { MatNativeDateModule } from '@angular/material/core';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogConfig, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+// import { Todo } from 'src/app/models/todo';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatDatepickerModule ],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent {
 
+export class InputComponent {
+  description: string = "some";
+  form!: FormGroup;
+
+  constructor(
+      private dialog: MatDialog,
+      private fb: FormBuilder,
+      private dialogRef: MatDialogRef<InputComponent>,
+      @Inject(MAT_DIALOG_DATA) data: any
+    ) {
+      this.description = data.description;
+    }
+
+  ngOnInit() {
+      this.form = this.fb.group({
+          description: [this.description, []],
+          
+      });
+  }
+
+  openDialog() {
+
+      const dialogConfig = new MatDialogConfig();
+
+      // dialogConfig.disableClose = true;
+      // dialogConfig.panelClass = true;
+      dialogConfig.autoFocus = true;
+      dialogConfig.hasBackdrop = true;
+
+
+      this.dialog.open(InputComponent, dialogConfig);
+  }
+
+  onClick() {
+
+  }
+  
+  save() {
+        // this.dialogRef.close();
+    }
+
+  close() {
+        this.dialogRef.close();
+    }
 }
